f5113ca3c156e3611e8561f1138bd7e5abc078fc
user macbook <usermacbook@users-MacBook-Pro.local>
2022-10-14 18:27:43 -0400

    HW3

---
+++ Modules/HW3.js
@@ -0,0 +1,14 @@
+let countryName = ['Japan']
+let countryName_split_nothing = countrtName.split('')
+console.log(`split by -> ${countryName_split_nothing}`);
+console.log(`Length -> ${countryName_split_nothing.length}`)
+
+const sentence2 = "HeAlTh wAs EArlIer said To Be the AbILitY of the bOdY funcTiOnInG WElL."
+let sentence2_split_space = sentence2.split(' ')
+console.log()
+
+
+
+const sentence3 = 'you Never waLK ALONe'
+let sentence3_uppercase =sentence3.toUpperCase().split('')
+console.log(sentence3_uppercase)
