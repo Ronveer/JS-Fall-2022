1c6b9b98a1f8309e624e610d719b955e5a1ce2ae
user macbook <usermacbook@users-MacBook-Pro.local>
2022-11-10 12:16:00 -0500

    Project-1.draft

---
+++ Modules/Project-1/Change-Course.js
@@ -0,0 +1,19 @@
+//const Students = require("./Students");
+const Institute = require("./Institute");
+const Faculties = require("./Faculties")
+
+function changeCourse(sId, newCourseName) {
+    console.log(this.Faculties.studentData.sId)
+
+//   changeAddress(id, newAddress) {
+//     if (id === this.studentData.sId) {
+//         this.memberData.mAddress = newAddress;
+//     } else {
+//         console.log('Invalid id provided')
+//     }
+// }
+//console.log("text")    // if (sId === this.studentData.sId) {
+    //     if this.memberDat
+    // }
+}
+console.log(changeCourse(2210, 'WE'))
